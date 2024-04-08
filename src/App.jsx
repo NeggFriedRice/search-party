8f499db962969a772b7b03268b848f74b1a58e4c
NeggFriedRice <tomloo222@gmail.com>
2024-04-08 12:34:29 +1000

    Add search input bar to search Stack Overflow

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ function App() {
 
   
   const [ stackResults , setStackResults] = useState(null)
+  const [ searchTerm, setSearchTerm ] = useState(null)
 
-  useEffect(() => {
-      searchStack()
-  }, [])
+  // useEffect(() => {
+  //     searchStack()
+  // }, [])
 
   async function searchStack() {
       try {
-      await fetch('https://api.stackexchange.com/2.3/search?order=desc&sort=activity&intitle=javascript&site=stackoverflow')
+      await fetch(`https://api.stackexchange.com/2.3/search?order=desc&sort=activity&intitle=${searchTerm}&site=stackoverflow`)
       .then( data => data.json())
       .then( response => setStackResults(response.items))
       } catch (err) {
@@ -20,9 +21,24 @@ function App() {
       }
   }
 
+  // Re-run API call with new search term
+  function searchSubmit(event) {
+    event.preventDefault()
+    searchStack()
+  }
+
+  // Handle search term input
+  function inputChangeHandler(event) {
+    setSearchTerm(event.target.value)
+  }
+
   return (
     <>
       <p>Hello</p>
+      <form onSubmit={searchSubmit}>
+        <input type="text" placeholder="Search here" value={searchTerm} onChange={inputChangeHandler}></input>
+        <button type="submit">Search</button>
+      </form>
       <StackSearch stackResults={stackResults} />
     </>
   )
