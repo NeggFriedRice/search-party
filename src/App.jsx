6612d756592a1049aa999f9d706b7508860f54a9
NeggFriedRice <tomloo222@gmail.com>
2024-04-08 12:14:14 +1000

    Move stackSearch function to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import StackSearch from './components/StackSearch'
 
 function App() {
 
+  
+  const [ stackResults , setStackResults] = useState(null)
+
+  useEffect(() => {
+      searchStack()
+  }, [])
+
+  async function searchStack() {
+      try {
+      await fetch('https://api.stackexchange.com/2.3/search?order=desc&sort=activity&intitle=javascript&site=stackoverflow')
+      .then( data => data.json())
+      .then( response => setStackResults(response.items))
+      } catch (err) {
+          console.log(err)
+      }
+  }
+
   return (
     <>
       <p>Hello</p>
-      <StackSearch />
+      <StackSearch stackResults={stackResults} />
     </>
   )
 }
--- src/components/StackSearch.jsx
+++ src/components/StackSearch.jsx
@@ -1,29 +1,15 @@
 import React, { useEffect, useState } from 'react'
 
-export default function StackSearch() {
-
-    const [ stackResults , setStackResults] = useState(null)
-
-    useEffect(() => {
-        searchStack()
-    }, [])
-
-    async function searchStack() {
-        try {
-        await fetch('https://api.stackexchange.com/2.3/search?order=desc&sort=activity&intitle=javascript&site=stackoverflow')
-        .then( data => data.json())
-        .then( response => setStackResults(response.items))
-        } catch (err) {
-            console.log(err)
-        }
-    }
+export default function StackSearch({stackResults}) {
 
     return (
         <>
-            <div>StackSearch</div>
-            { stackResults && 
+        <div className="bg-blue-800">
+            <div className="text-[26px]">StackSearch</div>
+            {stackResults && 
             stackResults.slice(0, 5).map((entry, index) =>
                 <p key={index}>{entry.title}</p>)}
+        </div>
         </>
     )
 }
